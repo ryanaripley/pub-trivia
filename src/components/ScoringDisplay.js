7a80c4d861306ca39fc790eb05f792c7f80e9177
Ryan A. Ripley <ryanaripley@gmail.com>
2019-02-08 19:57:23 -0600

    Add range input to scoring display

--- src/components/ScoringDisplay.js
+++ src/components/ScoringDisplay.js
@@ -3,39 +3,66 @@ import React, { Component } from 'react'
 import ScoreBoard from './ScoreBoard';
 
 export default class ScoringDisplay extends Component {
+
+  state = {
+    scoreChange: []
+  }
   
   async componentDidMount() {
+    const scoreChange = [];
+    this.props.players.forEach(player => {
+      scoreChange.push(0);
+    });
+    this.setState({ scoreChange });
   }
 
   componentWillUnmount() {
   }
 
-  processNextQuestion = () => {
-    this.deleteCurrentQuestion();
+  processScoring = () => {
+    this.props.players.forEach((player, index) => {
+      const score = this.state.scoreChange[index];
+      this.props.updatePlayerScore(index, this.state.scoreChange[index]);
+    });
+    this.props.processNextQuestion();
   }
 
   updateScore = (event) => {
-    const { index, score } = event.target.dataset;
-    const scoreInt = parseInt(score, 10);
-    this.props.updatePlayerScore(index, scoreInt);
+    event.target.classList.remove("positive", "negative")
+    const val = parseInt(event.target.value);
+    if (val === 1) {
+      event.target.classList.add("positive");
+    }
+    if (val === -1) {
+      event.target.classList.add("negative");
+    }
+    const { scoreChange } = this.state;
+    const { index } = event.target.dataset;
+    const score = parseInt(val, 10);
+    scoreChange[index] = score;
+    this.setState({ scoreChange });
   }
 
   render() {
     return (
       <div className="Scoring-display">
         <p>Score the previous question:</p>
-        <ul>
           {this.props.players.map((player, index) => {
           return (
-            <li key={index}>
-              <button data-index={index} data-score="-1" onClick={this.updateScore}>&#8722;</button>
-              <div className="scoring-name">{player.name}</div> 
-              <button data-index={index} data-score="1" onClick={this.updateScore}>&#43;</button>
-            </li>
+            <div key={index}>
+              <label>
+                <div className="scoring-name">{player.name}</div>
+                <div className="scores">
+                  <span>-1</span>
+                  <span>0</span>
+                  <span>+1</span>
+                </div>
+              </label>
+              <input data-index={index} type="range" min="-1" max="1" defaultValue="0" className="score-slider" step="1" onChange={this.updateScore} ></input>
+            </div>
           )
           })}
-        </ul>
-        <button onClick={this.props.processNextQuestion} className="next-question">Next question &rarr;</button>
+        <button onClick={this.processScoring} className="next-question">Next question &rarr;</button>
       </div>
     )
   }
